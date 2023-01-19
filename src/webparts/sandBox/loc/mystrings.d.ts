70d814665c08eb89ba29bb33fac4ea67faf71dd0
Arnaud André <aandre@webexpr.fr>
2023-01-19 10:32:22 +0100

    setup

---
+++ src/webparts/sandBox/loc/mystrings.d.ts
@@ -0,0 +1,18 @@
+declare interface ISandBoxWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+}
+
+declare module 'SandBoxWebPartStrings' {
+  const strings: ISandBoxWebPartStrings;
+  export = strings;
+}
